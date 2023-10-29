35d1ba31d36f5b39fe513b6006b7853af6352e46
Marco <marco.antonio@santolin.com.br>
2023-10-28 21:44:39 -0300

    Primeiro commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stack> //importando biblioteca stack -- pilha 
+#include "desenhos.cpp"
+using namespace std;
+
+int main() {
+    stack<int> area_estacionamento;
+    stack<int> area_manobra;
+    
+    
+    cppPark();
+
+    return 0;
+}
