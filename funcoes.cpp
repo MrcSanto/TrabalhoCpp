8bd765aa900bc0f25d93486d7a8cf2dc92bde5e2
Marco <marco.antonio@santolin.com.br>
2023-11-02 17:56:30 -0300

    Ajustes Finais

--- funcoes.cpp
+++ funcoes.cpp
@@ -2,7 +2,7 @@
 #include <stack>
 using namespace std;
 
-void cppPark(){
+void cppPark(){ // funcao para imprimir a logo cppPark
     cout <<
     "                     _____                _\n"
     "                    |  __ \\              | |\n"
@@ -17,49 +17,50 @@ void cppPark(){
 
 }
 
-int tamanho(stack<string> pilha){
-    int contador = 0;
+int tamanho(stack<string> pilha){ //funcao int para retornar o tamanho de uma pilha
+    int contador = 0;             // vai ser usado na main e em outras funçoẽs
     while(!pilha.empty()){
         pilha.pop();
-        contador++;
+        contador++;             //usa passagem por parametro para nao afetar as pilhas originais
     }
     return contador -1 ;
 }
 
-void invertePilha(stack<string> pilha1, stack<string> pilha2 ){ // funcao para inverter ou restaurar uma pilha
+void restauraPilha(stack<string> &pilha1, stack<string> &pilha2 ){ // funcao para restaurar uma pilha
     while(!pilha1.empty()){        
         string topo = pilha1.top();
-        pilha2.push(topo);
+        pilha2.push(topo);                  //serve para quando desempilhar uma pilha1 empilhar na pilha 2 novamente
         pilha1.pop();
     }
 }
     
-bool verificaCarro(const stack<string> &estacionamento, string placa){
+bool verificaCarro(const stack<string> &estacionamento, string placa){ //funcao para verificar se no input de placas há alguma repetida
     stack<string> pilha = estacionamento;
     while(!pilha.empty()){
         string topo = pilha.top();
         pilha.pop();
-        if(placa == topo){
+        if(placa == topo){ //aqui é feita a verificação das placas, se for true tem carros iguais e pede para inserir novamente
             return true;
         }
     }
-    return false;
+    return false; // se for falso nao tem carros iguais, logo o programa continua a inserir os dados
 }
 
-void retiraCarro(stack<string> &estacionamento, stack<string> &manobra, string placa){
-    if(verificaCarro(estacionamento, placa)){
+void retiraCarro(stack<string> &estacionamento, stack<string> &manobra, string placa){ // funcao usada para retirar os carros do estacionamento
+    if(verificaCarro(estacionamento, placa)){ // usa a funcao verificaCarro para validar se existe tal carro no input
         while(!estacionamento.empty()){
             string topo = estacionamento.top();
-            if(placa == topo){
-               cout << "Tem " << tamanho(manobra)+1 << " carro(s) na sua frente para retirar." << endl;
-               estacionamento.pop();
+            if(placa == topo){ 
+               cout << "Tem " << tamanho(manobra)+1 << " carro(s) na sua frente para retirar." << endl; //usa funcao tamanho
+               estacionamento.pop(); // carro nao esta mais na area de manobra, foi embora
                break;
             }
             manobra.push(topo); //adiciona a outra pilha
             estacionamento.pop();
        }
-       restauraPilha(manobra, estacionamento);
+       restauraPilha(manobra, estacionamento); // por meio da função restauraPilha é inserido os carros da Área de manobra para a Área de estacionamento
     }else{
-        cout << placa << "nao esta estacionado aqui" << endl;
+        cout << "O carro " << placa << " não esta estacionado aqui!" << endl;
+        cout << endl;
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <stack> //importando biblioteca stack -- pilha 
-#include "funcoes.cpp"
+#include "funcoes.cpp" // importa a minha biblioteca de funçoes
 using namespace std;
 
 int main() {
@@ -8,38 +8,40 @@ int main() {
     int opcao; 
     cppPark();
 
-    while(true){
+    while(true){ //cria um loop 
         
+        cout << "==============================\n";
         cout << "*  0 Para Sair\n";
         cout << "*  1 Registrar Entrada\n";
         cout << "*  2 Registrar Saida\n";
         cout << "*  3 Consultar Vaga\n";
+        cout << "==============================\n";
         cout << endl;
 
-        cin >> opcao;
+        cin >> opcao; // entrada da opção selecionada
 
         string placaCarro; //variavel da placa do carro
 
-        switch(opcao){
+        switch(opcao){ //vou usar switchCase para ficar mais organizado
             case 0:
                 cout << "Obrigado!!" << endl;
                 return 0;
             case 1:
                 {
-                    if(estacionamento.size() == 8){
+                    if(estacionamento.size() == 8){ //se estacionamento estiver cheio nao permite entrada de carros
                         cout << "Não há mais vagas!!" << endl;
                         cout << endl;
                         break;
                     }else{
-                        int size = 8 - estacionamento.size();
-                        cout << "Ainda há " << size << " vagas sobrando!!\nInsira a placa do seu carro:";
-                        cin.ignore();
+                        int size = 8 - estacionamento.size(); //cria uma varivel para quantos carros ainda podem entrar no estacionamento
+                        cout << "Ainda há " << size << " vagas sobrando!!\nInsira a placa do seu carro: ";
+                        cin.ignore(); //ignora qualquer caracter que tenha ficado 'esperando'
                         getline(cin, placaCarro);
-                        if(verificaCarro(estacionamento, placaCarro)){ //ver dps funciona parcialmente
+                        if(verificaCarro(estacionamento, placaCarro)){ //usa funcao, se for true executa as linhas de codigo abaixo
                             cout << "Ja existe uma carro igual! Tente novamente." << endl;
                             cout << endl;
                             break;
-                        }else{
+                        }else{ // se for false, da continuidade a entrada de informações do carro
                             estacionamento.push(placaCarro);
                             cout << endl;
                         }
@@ -48,51 +50,41 @@ int main() {
                 break;
             case 2:
                 {
-                    if(estacionamento.empty()){
+                    if(estacionamento.empty()){ // se estacionamento estiver vazio não tem nada para tirar
                         cout << "Estacionamento Vazio!!" << endl;
                         cout << endl;
                         break;
-                    }else{
-
-                        cout << "Insira a placa do carro a ser retirado:";
+                    }else{ // se tiver carros, executa as linhas abaixo
+                        cout << "Insira a placa do carro a ser retirado: ";
                         cin >> placaCarro;
-                        retiraCarro(estacionamento, manobra, placaCarro);
+                        retiraCarro(estacionamento, manobra, placaCarro); // os valores estao sendo passados por REFERENCIA
                     }
                 }
                 break;
             case 3:
                 {
-                    stack<string> temp;
+                    stack<string> temp; //cria pilha temporaria para inverter a ordem dos carros -- fica melhor para a saída dos dados
                     while(!estacionamento.empty()){
                         string topo = estacionamento.top();
                         temp.push(topo);
                         estacionamento.pop();
                     }
 
-                    int contador = 1;
+                    int contador = 1; //contador para exibir a ordem de chegada
                     while(!temp.empty()){
                         string topo = temp.top();
                         cout << contador++ << "°: " << topo << " --->  (" << tamanho(temp) << " carros na frente)" << endl;
                         temp.pop();
-                        estacionamento.push(topo);
+                        estacionamento.push(topo); // aqui vai retirando os elementos da pilha temp, ate nao sobrar mais nada
                     }
                     cout << endl;
                 }
                 break;
-            case 4:
-                while(!manobra.empty()){
-
-                    string topo = manobra.top();
-                    cout << topo << endl;
-                    manobra.pop();
-                }
-                break;
             default:
-                cout << "Opcao incorreta, tente novamente!!" << endl;;
+                cout << "Opcao incorreta, tente novamente!!" << endl;
+                cout << endl;
                 break;
         }
-
-
     }
     return 0;
 }
